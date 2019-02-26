091af0cfe2a6d6fdf1876b754d16cc4ab7893db5
tapnair <prrainsberry@yahoo.com>
2019-02-25 22:04:12 -0500

    added tree view

--- AppearanceUtility.py
+++ AppearanceUtility.py
@@ -47,42 +47,42 @@ cmd = {
 }
 command_definitions.append(cmd)
 
-# # Define parameters for 2nd command
-# cmd = {
-#     'cmd_name': 'Fusion Palette Demo Command',
-#     'cmd_description': 'Fusion Demo Palette Description',
-#     'cmd_id': 'cmdID_palette_demo_1',
-#     'cmd_resources': './resources',
-#     'workspace': 'FusionSolidEnvironment',
-#     'toolbar_panel_id': 'SolidScriptsAddinsPanel',
-#     'command_visible': True,
-#     'command_promoted': False,
-#     'palette_id': 'demo_palette_id',
-#     'palette_name': 'Demo Palette Name',
-#     'palette_html_file_url': 'demo.html',
-#     'palette_is_visible': True,
-#     'palette_show_close_button': True,
-#     'palette_is_resizable': True,
-#     'palette_width': 500,
-#     'palette_height': 600,
-#     'class': DemoPaletteShowCommand
-# }
-# command_definitions.append(cmd)
-#
-# # Define parameters for 2nd command
-# cmd = {
-#     'cmd_name': 'Fusion Palette Send Command',
-#     'cmd_description': 'Send info to Fusion 360 Palette',
-#     'cmd_id': 'cmdID_palette_send_demo_1',
-#     'cmd_resources': './resources',
-#     'workspace': 'FusionSolidEnvironment',
-#     'toolbar_panel_id': 'SolidScriptsAddinsPanel',
-#     'command_visible': True,
-#     'command_promoted': False,
-#     'palette_id': 'demo_palette_id',
-#     'class': DemoPaletteSendCommand
-# }
-# command_definitions.append(cmd)
+# Define parameters for 2nd command
+cmd = {
+    'cmd_name': 'Fusion Palette Demo Command',
+    'cmd_description': 'Fusion Demo Palette Description',
+    'cmd_id': 'cmdID_palette_demo_1',
+    'cmd_resources': './resources',
+    'workspace': 'FusionSolidEnvironment',
+    'toolbar_panel_id': 'SolidScriptsAddinsPanel',
+    'command_visible': True,
+    'command_promoted': False,
+    'palette_id': 'demo_palette_id',
+    'palette_name': 'Demo Palette Name',
+    'palette_html_file_url': 'demo.html',
+    'palette_is_visible': True,
+    'palette_show_close_button': True,
+    'palette_is_resizable': True,
+    'palette_width': 500,
+    'palette_height': 600,
+    'class': DemoPaletteShowCommand
+}
+command_definitions.append(cmd)
+
+# Define parameters for 2nd command
+cmd = {
+    'cmd_name': 'Fusion Palette Send Command',
+    'cmd_description': 'Send info to Fusion 360 Palette',
+    'cmd_id': 'cmdID_palette_send_demo_1',
+    'cmd_resources': './resources',
+    'workspace': 'FusionSolidEnvironment',
+    'toolbar_panel_id': 'SolidScriptsAddinsPanel',
+    'command_visible': True,
+    'command_promoted': False,
+    'palette_id': 'demo_palette_id',
+    'class': DemoPaletteSendCommand
+}
+command_definitions.append(cmd)
 
 # Set to True to display various useful messages when debugging your app
 debug = False
--- DemoPaletteCommand.py
+++ DemoPaletteCommand.py
@@ -3,9 +3,110 @@ import adsk.fusion
 import traceback
 
 import json
+from json import dumps
+from collections import defaultdict
 
 from .Fusion360Utilities.Fusion360CommandBase import Fusion360CommandBase, Fusion360PaletteCommandBase
 from .Fusion360Utilities.Fusion360Utilities import AppObjects
+
+
+def add_appearances_to_tree(node_list):
+    ao = AppObjects()
+
+    all_appearances = ao.design.appearances
+    for appearance in all_appearances:
+        used_by = appearance.usedBy
+        for item in used_by:
+
+            if item.objectType == adsk.fusion.BRepBody.classType():
+                body = {
+                    'id': item.assemblyContext.fullPathName + " - " + item.name,
+                    'name': item.name,
+                    'text': item.name,
+                    'parent': item.assemblyContext.name
+                }
+                node_list.append(body)
+
+                body_appearance = {
+                    'id': item.assemblyContext.fullPathName + " - " + item.name + " - " + appearance.name,
+                    'name': appearance.name,
+                    'text': appearance.name,
+                    'parent': item.assemblyContext.fullPathName + " - " + item.name
+                }
+                node_list.append(body_appearance)
+
+            elif item.objectType == adsk.fusion.BRepFace.classType():
+
+                # TODO check for duplicate bodies
+                body = {
+                    'id': item.assemblyContext.fullPathName + " - " + item.body.name,
+                    'name': item.body.name,
+                    'text': item.body.name,
+                    'parent': item.assemblyContext.name
+                }
+                node_list.append(body)
+
+                face = {
+                    'id': item.assemblyContext.fullPathName + " - " + str(item.tempId),
+                    'name': "Face" + " - " + str(item.tempId),
+                    'text': "Face" + " - " + str(item.tempId),
+                    'parent': item.assemblyContext.fullPathName + " - " + item.body.name
+                }
+                node_list.append(face)
+
+                face_appearance = {
+                    'id': item.assemblyContext.fullPathName + " - " + str(item.tempId) + " - " + appearance.name,
+                    'name': appearance.name,
+                    'text': appearance.name,
+                    'parent': item.assemblyContext.fullPathName + " - " + str(item.tempId)
+                }
+                node_list.append(face_appearance)
+
+            elif item.objectType == adsk.fusion.Occurrence.classType():
+                result = {
+                    'id': item.name + " - " + appearance.name,
+                    'name': appearance.name,
+                    'text': appearance.name,
+                    'parent': item.name
+                }
+
+                node_list.append(result)
+
+
+def make_component_tree():
+    ao = AppObjects()
+
+    node_list = []
+    root_node = {
+        'id': ao.root_comp.name,
+        'name': ao.root_comp.name,
+        'text': ao.root_comp.name,
+        'parent': "#"
+    }
+    node_list.append(root_node)
+
+    if ao.root_comp.occurrences.count > 0:
+        make_assembly_nodes(ao.root_comp.occurrences, node_list, ao.root_comp.name)
+
+    return node_list
+
+
+def make_assembly_nodes(occurrences: adsk.fusion.OccurrenceList, node_list, parent):
+
+    for occurrence in occurrences:
+        if occurrence.childOccurrences.count > 0:
+            make_assembly_nodes(occurrence.childOccurrences, node_list, occurrence.name)
+
+        make_node(occurrence, node_list, parent)
+
+
+def make_node(occurrence: adsk.fusion.Occurrence, node_list, parent):
+    node_list.append({
+        'id': occurrence.name,
+        'name': occurrence.name,
+        'text': occurrence.name,
+        'parent': parent
+    })
 
 
 # Class for a Fusion 360 Palette Command
@@ -57,7 +158,29 @@ class DemoPaletteSendCommand(Fusion360CommandBase):
 
         # Send message to the HTML Page
         if palette:
-            palette.sendInfoToHTML('send', message)
+            # palette.sendInfoToHTML('send', message)
+
+            # TODO build the tree
+
+            item1 = {
+                'id': "123",
+                'name': "name 1",
+                'text': "balh blah",
+                'parent': "#"
+            }
+            item2 = {
+                'id': "1234",
+                'name': "name 2",
+                'text': "balh blahcdcdsscsdcsd",
+                'parent': "123"
+            }
+            items_list = [item1, item2]
+
+            node_list = make_component_tree()
+            add_appearances_to_tree(node_list)
+            return_json = {'core': node_list, 'root_name': "the_root"}
+
+            palette.sendInfoToHTML('tree_update', dumps(return_json))
 
     # Run when the user selects your command icon from the Fusion 360 UI
     def on_create(self, command: adsk.core.Command, command_inputs: adsk.core.CommandInputs):
